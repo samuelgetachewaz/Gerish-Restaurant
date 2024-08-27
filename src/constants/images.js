2214101cf778f2f2f8df53d782422a06c7100702
samuelgetachewaz <bboysami063@gmail.com>
2024-08-27 23:04:49 +0300

    half_of_the_project

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,16 +1,16 @@
-//import bg from '../assets/bg.png';
-// import chef from '../assets/chef.png';
+import bg from '../assets/bg.png';
+import chef from '../assets/chef.png';
 import G from '../assets/G.png';
-// import gallery01 from '../assets/gallery01.png';
-// import gallery02 from '../assets/gallery02.png';
-// import gallery03 from '../assets/gallery03.png';
-// import gallery04 from '../assets/gallery04.png';
+import gallery01 from '../assets/gallery01.png';
+import gallery02 from '../assets/gallery02.png';
+import gallery03 from '../assets/gallery03.png';
+import gallery04 from '../assets/gallery04.png';
 import knife from '../assets/knife.png';
 import logo from '../assets/logo.png';
-// import menu from '../assets/menu.png';
+import menu from '../assets/menu.png';
 import overlaybg from '../assets/overlaybg.png';
 import spoon from '../assets/spoon.svg';
-// import welcome from '../assets/welcome.png';
+import welcome from '../assets/welcome.png';
 import findus from '../assets/findus.png';
 import laurels from '../assets/laurels.png';
 import award01 from '../assets/award01.png';
@@ -22,19 +22,19 @@ import quote from '../assets/quote.png';
 import gericht from '../assets/gericht.png';
 
 export default {
-  // bg,
-  // chef,
+  bg,
+  chef,
   G,
-  // gallery01,
-  // gallery02,
-  // gallery03,
-  // gallery04,
+  gallery01,
+  gallery02,
+  gallery03,
+  gallery04,
   knife,
   logo,
-  // menu,
+  menu,
   overlaybg,
   spoon,
-  // welcome,
+  welcome,
   findus,
   laurels,
   award01,
